abfd17e94e2b9ef092f13bfa37a3e66b995a7816
Miguelito223 <miguelitojimenez650@gmail.com>
2022-07-13 15:24:35 +0200

    v9.2

--- lua/entities/gd_w6_freezeray.lua
+++ lua/entities/gd_w6_freezeray.lua
@@ -111,7 +111,7 @@ end
 
 function ENT:AffectNpcs()
 	for k, v in pairs(ents.GetAll()) do
-		if v:IsNPC() v:IsNextBot() then 
+		if v:IsNPC() or v:IsNextBot() then 
 			if math.random(1,5)== 1 then
 				
 				InflictDamage(v, self, "cold", math.random	(15,20))
