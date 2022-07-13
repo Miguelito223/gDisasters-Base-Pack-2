4b9c2e384141dfe3a0415b40a273c8b9e8d555e3
odvigajlo <103889327+odvigajlo@users.noreply.github.com>
2022-07-13 21:17:33 +0300

    V9.4

---
+++ lua/entities/gd_w6_neptune.lua
@@ -0,0 +1,135 @@
+AddCSLuaFile()
+
+DEFINE_BASECLASS( "base_anim" )
+
+ENT.Spawnable		            	 = false        
+ENT.AdminSpawnable		             = false 
+
+ENT.PrintName		                 =  "Neptune"
+ENT.Author			                 =  "Hmm"
+ENT.Contact		                     =  "Hmm"
+ENT.Category                         =  "Hmm"
+
+ENT.Model                            =  "models/props_junk/PopCan01a.mdl"                      
+ENT.Mass                             =  100
+
+function ENT:Initialize()		
+
+	
+	if (SERVER) then
+	
+		GLOBAL_SYSTEM_TARGET =  {["Atmosphere"] 	= {["Wind"]        = {["Speed"]=math.random(1700,2000),["Direction"]=Vector(0,0,0)}, ["Pressure"]    = 10000, ["Temperature"] = math.random(-324,-355), ["Humidity"]    = math.random(0,0), ["BRadiation"]  = 0.1}}
+
+		self:SetModel(self.Model)
+		self:PhysicsInit( SOLID_VPHYSICS )
+		self:SetSolid( SOLID_VPHYSICS )
+		self:SetMoveType( MOVETYPE_NONE  )
+		self:SetUseType( ONOFF_USE )
+		self:SetCollisionGroup(COLLISION_GROUP_IN_VEHICLE)
+		
+		local phys = self:GetPhysicsObject()
+		if (phys:IsValid()) then
+			phys:SetMass(self.Mass)
+		end 
+		
+		self.Original_SkyData = {}
+			self.Original_SkyData["TopColor"]    = Vector(0.1, 0.1, 0.1)
+			self.Original_SkyData["BottomColor"] = Vector(0.1, 0.1, 0.1)
+			self.Original_SkyData["DuskScale"]   = 0
+			
+		self.Reset_SkyData    = {}
+			self.Reset_SkyData["TopColor"]       = Vector(0.20,0.50,1.00)
+			self.Reset_SkyData["BottomColor"]    = Vector(0.80,1.00,1.00)
+			self.Reset_SkyData["DuskScale"]      = 1
+			self.Reset_SkyData["SunColor"]       = Vector(0.20,0.10,0.00)
+		
+		for i=0, 100 do
+			timer.Simple(i/100, function()
+				if !self:IsValid() then return  end
+				paintSky_Fade(self.Original_SkyData, 0.05)
+			end)
+		end
+		
+	setMapLight("d")		
+	
+
+
+	local data = {}
+		data.Color = Color(115,115,185)
+		data.DensityCurrent = 0
+		data.DensityMax     = 0.2
+		data.DensityMin     = 0.1
+		data.EndMax         = 2000
+		data.EndMin         = 1000
+		data.EndMinCurrent  = 0
+		data.EndMaxCurrent  = 0       
+
+	gDisasters_CreateGlobalFog(self, data, true)	
+	
+	gDisasters_CreateGlobalGFX("coldwave", self)						
+		
+	end
+end
+
+function ENT:SpawnFunction( ply, tr )
+	if ( !tr.Hit ) then return end
+
+	self.OWNER = ply
+	local ent = ents.Create( self.ClassName )
+	ent:SetPhysicsAttacker(ply)
+	ent:SetPos( tr.HitPos + tr.HitNormal * 16 ) 
+	ent:Spawn()
+	ent:Activate()
+	return ent
+end
+
+function ENT:AffectNpcs()
+	for k, v in pairs(ents.GetAll()) do
+		if v:IsNPC() then 
+			if math.random(1,2)== 1 then
+				
+				InflictDamage(v, self, "cold", math.random	(2,5))
+
+			end	
+		end
+	end	
+end	
+
+function ENT:Think()
+	if (SERVER) then
+		if !self:IsValid() then return end
+		self:AffectNpcs()
+		self:NextThink(CurTime() + 0.01)
+		return true
+	end
+end
+
+function ENT:OnRemove()
+
+	if (SERVER) then		
+		local resetdata = self.Reset_SkyData
+		GLOBAL_SYSTEM_TARGET=GLOBAL_SYSTEM_ORIGINAL
+
+		for i=0, 40 do
+			timer.Simple(i/100, function()
+				paintSky_Fade(resetdata,0.05)
+			end)
+		end
+		
+		setMapLight("t")	
+	end
+	
+	
+end
+
+function ENT:UpdateTransmitState()
+
+	return TRANSMIT_ALWAYS
+
+end
+
+
+
+
+
+
