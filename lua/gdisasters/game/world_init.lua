7ea396eda6bc04aef7f359cc807bb45b2b3ce588
Miguelito223 <miguelitojimenez650@gmail.com>
2022-07-22 20:20:07 +0200

    mmm

--- lua/gdisasters/game/world_init.lua
+++ lua/gdisasters/game/world_init.lua
@@ -137,7 +137,7 @@ end)
 
 concommand.Add("body_temp", function(test, test2, test3)
 	for k, v in pairs(player.GetAll()) do
-	
+
 		v.BodyTemperature = tonumber(test3[1])
 	
 	end
@@ -156,40 +156,47 @@ end)
 
 hook.Add( "Initialize", "AtmosInitFix", function()
 
-	if #ents.FindByClass("env_skypaint")<1 then
+	if GetConVar("gdisasters_atmosphere"):GetInt() <= 0 then return end
+	
+	if GetConVar("gdisasters_atmosphere"):GetInt() <= 1 then 
+
+		if #ents.FindByClass("env_skypaint")<1 then
 		local ent = ents.Create("env_skypaint")
-		ent:SetPos(Vector(0,0,0))
-		ent:Spawn()
+			ent:SetPos(Vector(0,0,0))
+			ent:Spawn()
+		end
+
+		RunConsoleCommand( "sv_skyname", "painted" )
+
+		if ( game.ConsoleCommand ) then
+
+			game.ConsoleCommand( "sv_skyname painted\n" )
+
+		end
 	end
-
-	RunConsoleCommand( "sv_skyname", "painted" )
-
-	if ( game.ConsoleCommand ) then
-
-		game.ConsoleCommand( "sv_skyname painted\n" )
-
-	end
-
 end )
 
 hook.Add( "InitPostEntity", "gDisastersInitPostEvo", function()
 
 
-
+	if GetConVar("gdisasters_atmosphere"):GetInt() <= 0 then return end
+	
+	if GetConVar("gdisasters_atmosphere"):GetInt() <= 1 then 
 
 	local oldCleanUpMap = game.CleanUpMap
 	
-	game.CleanUpMap = function(dontSendToClients, ExtraFilters)
-		dontSendToClients = (dontSendToClients != nil and dontSendToClients or false)
+		game.CleanUpMap = function(dontSendToClients, ExtraFilters)
+			dontSendToClients = (dontSendToClients != nil and dontSendToClients or false)
 
-		if ( ExtraFilters != nil ) then
-			table.insert(ExtraFilters, "env_skypaint")
-			table.insert(ExtraFilters, "light_environment")
-		else
-			ExtraFilters = { "env_skypaint", "light_environment" }
+			if ( ExtraFilters != nil ) then
+				table.insert(ExtraFilters, "env_skypaint")
+				table.insert(ExtraFilters, "light_environment")
+			else
+				ExtraFilters = { "env_skypaint", "light_environment" }
+			end
+
+			oldCleanUpMap(dontSendToClients, ExtraFilters)
 		end
-
-		oldCleanUpMap(dontSendToClients, ExtraFilters)
 	end
 
 end )
