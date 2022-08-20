bf67d992d2decd79e246e6b363531c1501884e33
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2022-08-20 11:40:35 +0200

    dik bro

--- lua/gdisasters/stormfox/main.lua
+++ lua/gdisasters/stormfox/main.lua
@@ -6,7 +6,7 @@ function start()
     if GetConVar("gdisasters_stormfox_enable"):GetInt() == 0 then return end
     temp = StormFox2.Temperature.Get()
     wind = StormFox2.Wind.GetForce()
-    wind_direction = Vector(StormFox2.Wind.GetNorm().x,StormFox2.Wind.GetNorm().y,StormFox2.Wind.GetNorm().z)
+    wind_direction = Vector(StormFox2.Wind.GetYaw(), -StormFox2.Wind.GetYaw(), 0)
     
     GLOBAL_SYSTEM_TARGET["Atmosphere"]["Temperature"] = temp
 	GLOBAL_SYSTEM_TARGET["Atmosphere"]["Wind"]["Speed"] = wind
