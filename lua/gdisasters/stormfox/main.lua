171cf744ae16c9565493e34ddce673fcaeeca56f
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2022-08-20 20:01:07 +0200

    try to mix the wind direction with stormfox but I find it complicated :/

--- lua/gdisasters/stormfox/main.lua
+++ lua/gdisasters/stormfox/main.lua
@@ -6,11 +6,9 @@ function start()
     if GetConVar("gdisasters_stormfox_enable"):GetInt() == 0 then return end
     temp = StormFox2.Temperature.Get()
     wind = StormFox2.Wind.GetForce()
-    wind_direction = Vector(StormFox2.Wind.GetYaw(), -StormFox2.Wind.GetYaw(), 0)
     
     GLOBAL_SYSTEM_TARGET["Atmosphere"]["Temperature"] = temp
 	GLOBAL_SYSTEM_TARGET["Atmosphere"]["Wind"]["Speed"] = wind
-    GLOBAL_SYSTEM_TARGET["Atmosphere"]["Wind"]["Direction"] = wind_direction
 end
 hook.Add("Tick", "stormfoxandgdisasters", start)
 end
