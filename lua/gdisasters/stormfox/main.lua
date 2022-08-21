940d1531c0583931f9b7261a581dee5017efd9d2
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2022-08-21 12:19:37 +0200

    xd

--- lua/gdisasters/stormfox/main.lua
+++ lua/gdisasters/stormfox/main.lua
@@ -4,15 +4,15 @@ if (SERVER) then
 
 function start()
     if GetConVar("gdisasters_stormfox_enable"):GetInt() == 0 then return end
-    temp = StormFox2.Temperature.Get()
-    tempd = temp + -1
+    temp = StormFox2.Temperature.Get(pressure)
+    tempd = temp + 35 * (humidity / 100)
     wind = StormFox2.Wind.GetForce()
     pressure = 6.11 * 10 * (7.5 * temp / 237.3 + temp)
     pressurereal = 6.11 * 10 * (7.5 * tempd / 237.3 + tempd)
     humidity = pressure / pressurereal * 100
     alture = getMapCenterFloorPos().z
-    density = 1225
-    gravity = 9.8
+    density = physenv.GetAirDensity()
+    gravity = physenv.GetGravity()
     pressureATS = alture * density * gravity
     
     GLOBAL_SYSTEM_TARGET["Atmosphere"]["Temperature"] = temp
