6ab62e7dca097371b68c09167adca3429fffce3d
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2022-08-21 18:19:46 +0200

    ok...

--- lua/gdisasters/stormfox/main.lua
+++ lua/gdisasters/stormfox/main.lua
@@ -5,19 +5,9 @@ if (SERVER) then
 function start()
     if GetConVar("gdisasters_stormfox_enable"):GetInt() == 0 then return end
     temp = StormFox2.Temperature.Get(pressure)
-    tempd = temp + 35 * (humidity / 100)
     wind = StormFox2.Wind.GetForce()
-    pressure = 6.11 * 10 * (7.5 * temp / 237.3 + temp)
-    pressurereal = 6.11 * 10 * (7.5 * tempd / 237.3 + tempd)
-    humidity = pressure / pressurereal * 100
-    alture = getMapCenterFloorPos().z
-    density = physenv.GetAirDensity()
-    gravity = physenv.GetGravity()
-    pressureATS = alture * density * gravity
     
     GLOBAL_SYSTEM_TARGET["Atmosphere"]["Temperature"] = temp
-    GLOBAL_SYSTEM_TARGET["Atmosphere"]["Humidity"] = humidity
-    GLOBAL_SYSTEM_TARGET["Atmosphere"]["Pressure"] = pressureATS
 	GLOBAL_SYSTEM_TARGET["Atmosphere"]["Wind"]["Speed"] = wind
 end
 hook.Add("Tick", "stormfoxandgdisasters", start)
