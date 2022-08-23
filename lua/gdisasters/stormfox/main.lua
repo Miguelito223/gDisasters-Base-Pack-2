767967277e1bb3d0f923178578eee3f95daf4363
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2022-08-23 20:50:54 +0200

    better no

--- lua/gdisasters/stormfox/main.lua
+++ lua/gdisasters/stormfox/main.lua
@@ -4,13 +4,11 @@ if (SERVER) then
 
 function start()
     if GetConVar("gdisasters_stormfox_enable"):GetInt() == 0 then return end
-    if stormfox2 then
-        temp = StormFox2.Temperature.Get()
-        wind = StormFox2.Wind.GetForce()
+    temp = StormFox2.Temperature.Get()
+    wind = StormFox2.Wind.GetForce()
     
-        GLOBAL_SYSTEM_TARGET["Atmosphere"]["Temperature"] = temp
-	    GLOBAL_SYSTEM_TARGET["Atmosphere"]["Wind"]["Speed"] = wind
-    end
+    GLOBAL_SYSTEM_TARGET["Atmosphere"]["Temperature"] = temp
+	GLOBAL_SYSTEM_TARGET["Atmosphere"]["Wind"]["Speed"] = wind
 end
 hook.Add("Tick", "stormfoxandgdisasters", start)
 end
