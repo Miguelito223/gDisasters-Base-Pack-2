3cfff7d58d168282549dc2d7a1dd5353df17aa7d
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2022-08-23 20:39:20 +0200

    fix no detect if stormfox 1 or 2

--- lua/gdisasters/stormfox/main.lua
+++ lua/gdisasters/stormfox/main.lua
@@ -4,11 +4,13 @@ if (SERVER) then
 
 function start()
     if GetConVar("gdisasters_stormfox_enable"):GetInt() == 0 then return end
-    temp = StormFox2.Temperature.Get()
-    wind = StormFox2.Wind.GetForce()
+    if stormfox2 then
+        temp = StormFox2.Temperature.Get()
+        wind = StormFox2.Wind.GetForce()
     
-    GLOBAL_SYSTEM_TARGET["Atmosphere"]["Temperature"] = temp
-	GLOBAL_SYSTEM_TARGET["Atmosphere"]["Wind"]["Speed"] = wind
+        GLOBAL_SYSTEM_TARGET["Atmosphere"]["Temperature"] = temp
+	    GLOBAL_SYSTEM_TARGET["Atmosphere"]["Wind"]["Speed"] = wind
+    end
 end
 hook.Add("Tick", "stormfoxandgdisasters", start)
 end
