46ed055916ab8766a9fe8f2a8266916765bff098
Miguelito223 <miguelitojimenez650@gmail.com>
2022-08-28 09:18:07 +0200

    e

--- lua/gdisasters/stormfox/main.lua
+++ lua/gdisasters/stormfox/main.lua
@@ -10,9 +10,9 @@ function start()
     GLOBAL_SYSTEM_TARGET["Atmosphere"]["Temperature"] = temp
 	GLOBAL_SYSTEM_TARGET["Atmosphere"]["Wind"]["Speed"] = wind
 
-    if !StormFox2.Weather.IsRaining() and StormFox2.Weather.GetRainAmount(0) then
+    if !StormFox2.Weather.IsRaining() or !StormFox2.Weather.IsSnowing() and StormFox2.Weather.GetRainAmount(0) then
         GLOBAL_SYSTEM_TARGET["Atmosphere"]["Humidity"] = 0
-    elseif StormFox2.Weather.IsRaining() and StormFox2.Weather.GetRainAmount(1) then
+    elseif StormFox2.Weather.IsRaining() or StormFox2.Weather.IsSnowing() and StormFox2.Weather.GetRainAmount(1) then
         GLOBAL_SYSTEM_TARGET["Atmosphere"]["Humidity"] = 50
     end
 end
