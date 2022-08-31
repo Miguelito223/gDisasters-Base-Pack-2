05f157651bf7e8ac4dd734b25e5d2548728fab10
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2022-08-31 10:44:28 +0200

    idk

--- lua/gdisasters/experimental/heat-system.lua
+++ lua/gdisasters/experimental/heat-system.lua
@@ -10,77 +10,48 @@ function Core() -- System Core
 	airheat =  23
 	upperairheat =  15
 	waterheat =  0
-	heatIRmultiplier = 1
-	sunHeat = 1
-	heightCooldownRate = 1
-	pressureCoeffitient = 1000
-	lowerPointPressure = 10
-	CAPE = 0
-	windDiff = 0
+	steam = 0
+	humidity = 0
+	gravity = physenv.GetGravity()
+	centerpos = getMapCenterPos()
+	pressure = gravity * centerpos
+	capeofcondesation = 1000
+	SunHeat = 1
+	Cold = 1
+	
 
-	CalculateHeat()
+	local function SetGLOBALSYSTEM() -- Make Values Like Wind Speed Show Correct Stuff
+		GLOBAL_SYSTEM["Atmosphere"]["Temperature"] = airheat
+		GLOBAL_SYSTEM["Atmosphere"]["Humidity"] = humidity
+	end
+
+	local function calculateheat()
+		airheat = airheat + SunHeat - Cold
+		floorheat = floorheat + SunHeat * airheat
+		waterheat = waterheat + SunHeat * airheat
+
+		if floorheat and waterheat >= 20 then
+			steam = 15
+			humidity = 52
+		elseif floorheat and waterheat >= 30 then
+			steam = 28
+			humidity = 28
+		elseif floorheat and waterheat >= 10 then
+			steam = 8
+			humidity = 100
+		elseif floorheat and waterheat >= 10 then
+			steam = 8
+			humidity = 100
+		end
+
+
+
+	end
+
+	calculateheat()
 	SetGLOBALSYSTEM()
 	
 end
 hook.Add("Tick", "Experimental", Core)
-
-
-function CalculateHeat() -- Calculate Heat -_-
-	TransferHeatToGround()
-	AddHeatToGround()
-	CalculateAirInstability()
-	RandomEvent()
-	Weather()
-    airheat = airheat + sunHeat - heatIRmultiplier
-	upperairheat = airheat - (heatIRmultiplier * sunHeat) * heightCooldownRate
-	waterheat = floorheat + sunHeat - heatIRmultiplier
-
-end
-
-function CalculateAirInstability() -- Calculate CAPE index, wind
-
-    windDiff = airheat + waterheat / lowerPointPressure
-    CAPE = airheat + waterheat * (sunHeat + 1) + heatIRmultiplier + heightCooldownRate
-
-end
-
-function TransferHeatToGround() -- Trasfer Current Heat To Ground (Floor)
-    floorheat = airheat - heatIRmultiplier + sunHeat
-	airheat = airheat - heatIRmultiplier
-end
-
-function AddHeatToGround() -- Trasfer Current Heat To Ground (Floor)
-
-    floorheat = floorheat + sunHeat - heatIRmultiplier
-	waterheat = waterheat + sunHeat - heatIRmultiplier
-
-end
-
-function Weather() -- Weather Stuffy
-
-end
-
-function RandomEvent() -- Randomly Change Stuff For More Variance
-
-    if math.random(1, 256) == 11 then
-		airheat = airheat * math.random(-3, 3)
-	end
-
-	if math.random(1, 256) == 34 then
-		waterheat = waterheat * math.random(-3, 3)
-	end
-
-	if math.random(1, 256) == 84 then
-		upperairheat = upperairheat * math.random(-5, 5)
-	end
-
-end
-
-function SetGLOBALSYSTEM() -- Make Values Like Wind Speed Show Correct Stuff
-	GLOBAL_SYSTEM["Atmosphere"]["Temperature"] = airheat
-	GLOBAL_SYSTEM["Atmosphere"]["Pressure"] = airheat * pressureCoeffitient
-	GLOBAL_SYSTEM["Atmosphere"]["Humidity"] = waterheat + airheat
-	GLOBAL_SYSTEM["Atmosphere"]["Wind"]["Speed"] = windDiff
-end
 
 end
