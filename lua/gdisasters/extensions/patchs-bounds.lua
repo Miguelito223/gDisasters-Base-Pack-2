807a49937819f14d5b7d36e9eaec528c1b847f6d
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2022-10-17 10:50:44 +0200

    added gm_boreas

--- lua/gdisasters/extensions/patchs-bounds.lua
+++ lua/gdisasters/extensions/patchs-bounds.lua
@@ -85,6 +85,7 @@ MAP_BOUNDS["gmt_farmland_final"]  = { Vector(-15343, 15343, 64), Vector(15343,-1
 MAP_BOUNDS["gm_tornado_interstate_95"]  = { Vector(162710, 16271, -6052), Vector(-16271, -16271, -6052), Vector(0, 0, -6050)}
 MAP_BOUNDS["gm_tornadoalley_e1"]  = { Vector(-15343, -15338, 64), Vector(15300, -15331, 64), Vector(0, 0, 0)}
 MAP_BOUNDS["gm_teletubbyland_day_remake"]  = { Vector(-10415, 6267, -1215), Vector(10213, -8722, -1301), Vector(0, 0, -1215)}
+MAP_BOUNDS["gm_boreas"] = { Vector(-15855, -15855, -10308), Vector(15855, 15850, -10308), Vector(0, 0, -10414)}
 
 
 MAP_PATHS = {}
