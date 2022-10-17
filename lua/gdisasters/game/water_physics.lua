e6cb42d2ae769b8f52dbc9671619654416006b5b
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2022-10-17 11:30:23 +0200

    added more types of water float

--- lua/gdisasters/game/water_physics.lua
+++ lua/gdisasters/game/water_physics.lua
@@ -1,4 +1,5 @@
-Buoyancy_Lookup = { ["plastic_barrel"] = 5,
+Buoyancy_Lookup = { 
+					["plastic_barrel"] = 5,
 					["floating_metal_barrel"] = 5,
 					["metal"] = 0.2,
 					["metalpanel"] = 0.2,
@@ -6,6 +7,21 @@ Buoyancy_Lookup = { ["plastic_barrel"] = 5,
 					["wood_crate"] = 4,
 					["default"] = 1.00,
 					["wood"] = 5,
+					["flesh"] = 4,
+					["alienflesh"] = 4,
+					["bloodyflesh"] = 4,
+					["zombieflesh"] = 4,
+					["armorflesh"] = 4,
+					["paper"] = 5,
+					["gmod_bouncy"] = 5,
+					["metal_bouncy"] = 3,
+					["dirt"] = 3,
+					["glass"] = 4,
+					["concrete"] = 0.2,
+					["ice"] = 4,
+					["gmod_ice"] = 4,
+					["slime"] = 4,
+					["rubber"] = 5,
 					}
 
 					
