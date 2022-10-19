bab01341007597e32affca6aaec5dd678f56b659
Miguelito223 <miguelitojimenez650@gmail.com>
2022-10-19 15:04:25 +0200

    e

--- lua/gdisasters/player/cl_menu.lua
+++ lua/gdisasters/player/cl_menu.lua
@@ -168,7 +168,7 @@ local function gDisastersServerGraphics( CPanel )
 
 	AddControlLabel( CPanel, "Antilag options: remove the lag :)" )
 	
-	CreateSliderConVariable(CPanel,"Antilag Mode (s)", 0, 2, 0,"gdisasters_antilag_mode" );
+	--CreateSliderConVariable(CPanel,"Antilag Mode (s)", 0, 2, 0,"gdisasters_antilag_mode" );
 	CreateTickboxConVariable(CPanel,"Enable Antilag", "gdisasters_antilag_enabled" );
 	
 end
