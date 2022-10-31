229dbe96f046490bfe7acc697804b6f572667ad8
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2022-10-31 10:15:19 +0100

    ups

--- lua/entities/gd_w6_hypercane.lua
+++ lua/entities/gd_w6_hypercane.lua
@@ -157,7 +157,7 @@ function ENT:Think()
 			util.ScreenShake( LocalPlayer():GetPos(), 0.4, 5, 0.2, 500 )
 		end
 		
-		if math.random(1,2500)==1 then surface.PlaySound(streams/disasters/nature/thunder"..tostring(math.random(1,3))..".wav") end 
+		if math.random(1,2500)==1 then surface.PlaySound("streams/disasters/nature/thunder"..tostring(math.random(1,3))..".wav") end 
 		
 		
 	end
