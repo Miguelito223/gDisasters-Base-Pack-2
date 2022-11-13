31611a24805791e22cd88a8aa4bbba343276553e
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2022-11-13 12:48:00 +0100

    xd

--- lua/gdisasters/player/process_stormfox2.lua
+++ lua/gdisasters/player/process_stormfox2.lua
@@ -18,12 +18,16 @@ function gDisasters_stormfox2()
 
     if !StormFox2.Weather.IsRaining() and !StormFox2.Weather.IsSnowing() and StormFox2.Weather.GetRainAmount(0) then
         GLOBAL_SYSTEM_TARGET["Atmosphere"]["Humidity"] = 0
+        GLOBAL_SYSTEM_TARGET["Atmosphere"]["Pressure"] = 10200
     elseif StormFox2.Weather.IsRaining() and StormFox2.Weather.GetRainAmount(1) then
         GLOBAL_SYSTEM_TARGET["Atmosphere"]["Humidity"] = 100
+        GLOBAL_SYSTEM_TARGET["Atmosphere"]["Pressure"] = 9600
     elseif StormFox2.Weather.IsSnowing() and StormFox2.Weather.GetRainAmount(0)then
         GLOBAL_SYSTEM_TARGET["Atmosphere"]["Humidity"] = 100
+        GLOBAL_SYSTEM_TARGET["Atmosphere"]["Pressure"] = 9600
     else
         GLOBAL_SYSTEM_TARGET["Atmosphere"]["Humidity"] = 0
+        GLOBAL_SYSTEM_TARGET["Atmosphere"]["Pressure"] = 10200
     end
 
     if StormFox2.Thunder.IsThundering() then
