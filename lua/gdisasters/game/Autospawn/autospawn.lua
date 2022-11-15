b1adb5134213059246fd3e5503b3ee5f827ba9ab
Miguelito223 <miguelitojimenez650@gmail.com>
2022-11-15 16:20:07 +0100

    eh

--- lua/gdisasters/game/Autospawn/autospawn.lua
+++ lua/gdisasters/game/Autospawn/autospawn.lua
@@ -427,7 +427,7 @@ function Autospawn_Timer()
 					if recent then recent = false return end
 					Autospawn()
 				end
-			elseif IsMapRegistered() == true and S37K_mapbounds == nil or table.IsEmpty(S37K_mapbounds) then
+			elseif IsMapRegistered() == true and S37K_mapbounds == nil or table.IsEmpty(S37K_mapbounds) == true then
 				if math.random(0,GetConVar( "gdisasters_autospawn_spawn_chance" ):GetInt()) == GetConVar( "gdisasters_autospawn_spawn_chance" ):GetInt() then
 					if recent then recent = false return end
 					Autospawn()
