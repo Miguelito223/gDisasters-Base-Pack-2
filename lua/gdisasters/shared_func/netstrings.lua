92d512433dd6e548934c1576cc8e4ceccbe4036f
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2022-11-26 10:27:29 +0100

    ups

--- lua/gdisasters/shared_func/netstrings.lua
+++ lua/gdisasters/shared_func/netstrings.lua
@@ -189,14 +189,13 @@ if (CLIENT) then
 		local size     = net.ReadFloat()
 		local life     = net.ReadFloat() + CurTime()
 		
-		if GetConVar("disasters_graphics_enable_manual_number_of_screen_particles"):GetInt() >= 1 then  
+		if GetConVar("gdisasters_graphics_enable_manual_number_of_screen_particles"):GetInt() >= 1 then  
 			local number   = math.random(0, GetConVar("gdisasters_graphics_number_of_screen_particles"):GetFloat())
 		else
 			local number   = net.ReadFloat()
 		end
 		
 		local vel      = net.ReadVector()
-
 
 		for i=0, number do
 			local pos      = Vector( math.random(0,ScrW()), math.random(0,ScrH()), 0) 
