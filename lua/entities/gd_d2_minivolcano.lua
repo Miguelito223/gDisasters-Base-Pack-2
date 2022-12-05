3eed150bfe7c4352f1139042270340c4352cb1c1
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2022-12-05 16:42:18 +0100

    fixed mini volcano spawn

--- lua/entities/gd_d2_minivolcano.lua
+++ lua/entities/gd_d2_minivolcano.lua
@@ -269,7 +269,7 @@ function ENT:SpawnFunction( ply, tr )
 	self.OWNER = ply
 	local ent = ents.Create( self.ClassName )
 	ent:SetPhysicsAttacker(ply)
-	ent:SetPos( tr.HitPos + tr.HitNormal * 85  ) 
+	ent:SetPos( tr.HitPos + tr.HitNormal * 42.5  ) 
 	ent:Spawn()
 	ent:Activate()
 	return ent
