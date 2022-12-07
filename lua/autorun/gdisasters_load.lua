d871e0239d95941edcf5de9f490ebc04ca753906
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2022-12-07 13:23:58 +0100

    xd

--- lua/autorun/gdisasters_load.lua
+++ lua/autorun/gdisasters_load.lua
@@ -20,7 +20,7 @@ if (SERVER) then
 	AddCSLuaFile("gdisasters/atmosphere/main.lua")
 	AddCSLuaFile("gdisasters/hud/main.lua")	
 	AddCSLuaFile("gdisasters/game/decals.lua")
-	AddCSLuaFile("gdisasters/game/Autospawn/autospawn.lua")
+	AddCSLuaFile("gdisasters/game/autospawn.lua")
 
 	include("gdisasters/shared_func/main.lua")	
 	include("gdisasters/shared_func/netstrings.lua")
@@ -33,7 +33,7 @@ if (SERVER) then
 	include("gdisasters/player/postspawn.lua")
 	include("gdisasters/player/menu.lua")
 	include("gdisasters/game/decals.lua")
-	include("gdisasters/game/Autospawn/autospawn.lua")
+	include("gdisasters/game/autospawn.lua")
 	
 	include("gdisasters/spawnlist/menu/main.lua")
 	include("gdisasters/spawnlist/menu/populate.lua")
@@ -55,6 +55,26 @@ if (CLIENT) then
 	AddCSLuaFile("autorun/gdisasters_load.lua")
 	AddCSLuaFile("autorun/precached_particles.lua")
 
+	AddCSLuaFile("gdisasters/shared_func/main.lua")
+	AddCSLuaFile("gdisasters/shared_func/netstrings.lua")
+	AddCSLuaFile("gdisasters/extensions/patchs-bounds.lua")
+	AddCSLuaFile("gdisasters/game/convars/main.lua")
+	AddCSLuaFile("gdisasters/player/menu.lua")
+	AddCSLuaFile("gdisasters/player/postspawn.lua")
+	AddCSLuaFile("gdisasters/game/water_physics.lua")
+	AddCSLuaFile("gdisasters/game/world_init.lua")
+	AddCSLuaFile("gdisasters/spawnlist/menu/main.lua")
+	AddCSLuaFile("gdisasters/spawnlist/menu/populate.lua")
+	AddCSLuaFile("gdisasters/game/dnc.lua")
+	AddCSLuaFile("gdisasters/game/damagetypes.lua")
+	AddCSLuaFile("gdisasters/player/process_gfx.lua")
+	AddCSLuaFile("gdisasters/player/process_temp.lua")
+	AddCSLuaFile("gdisasters/player/process_oxygen.lua")
+	AddCSLuaFile("gdisasters/atmosphere/main.lua")
+	AddCSLuaFile("gdisasters/hud/main.lua")	
+	AddCSLuaFile("gdisasters/game/decals.lua")
+	AddCSLuaFile("gdisasters/game/autospawn.lua")
+
 	include("gdisasters/player/menu.lua")
 	include("gdisasters/shared_func/main.lua")	
 	include("gdisasters/shared_func/netstrings.lua")
@@ -67,7 +87,7 @@ if (CLIENT) then
 	
 	include("gdisasters/atmosphere/main.lua")
 	include("gdisasters/hud/main.lua")
-	include("gdisasters/game/Autospawn/autospawn.lua")
+	include("gdisasters/game/autospawn.lua")
 	include("gdisasters/spawnlist/menu/main.lua")
 	include("gdisasters/spawnlist/menu/populate.lua")
 
