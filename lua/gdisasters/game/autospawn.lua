57d67ad3466cb13b9efa8a3333f67ec07077c624
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2022-12-09 11:23:50 +0100

    aa

--- lua/gdisasters/game/autospawn.lua
+++ lua/gdisasters/game/autospawn.lua
@@ -21,7 +21,7 @@ if (SERVER) then
 				})
 
 				stormtableFloorPos = tr.HitPos
-				stormtableCenterPos = Vector(-stormtable.positiveX,-stormtable.positiveY,-stormtable.skyZ) + Vector(stormtable.positiveX,stormtable.positiveY,stormtable.skyZ) / 2
+				stormtableCenterPos = Vector(stormtable.negativeX,stormtable.negativeY,-stormtable.skyZ) + Vector(stormtable.positiveX,stormtable.positiveY,stormtable.skyZ) / 2
 			end
 
 			local SkyPos = Vector(math.random(map_bounds[1].x,map_bounds[2].x),  math.random(map_bounds[1].y,map_bounds[2].y),  map_skybox[2].z)
