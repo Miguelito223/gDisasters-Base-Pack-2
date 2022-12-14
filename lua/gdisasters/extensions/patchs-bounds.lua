7c54d3257de5f44dd07f3196df35b5824b40f7f4
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2022-12-14 19:05:17 +0100

    emm

--- lua/gdisasters/extensions/patchs-bounds.lua
+++ lua/gdisasters/extensions/patchs-bounds.lua
@@ -92,6 +92,9 @@ MAP_BOUNDS["freespace_gs"] = { Vector(15703, -15159, -3996), Vector(-14983, 1552
 MAP_BOUNDS["gm_tornado_province_40"] = { Vector(16173, 16173, 128), Vector(-16173, -16173, -3984), Vector(0, 0, -8)}
 MAP_BOUNDS["gm_arid_valley_v2_day"] = { Vector(-14313, 14319, -206), Vector(14319, -14319, 15482), Vector(0, 0, -206)}
 MAP_BOUNDS["gm_flatgrass_2021"] = { Vector(15359, 15359, -15359), Vector(-15359, -15359, 15359), Vector(0, 0, -12914)}
+MAP_BOUNDS["gm_warmap_v5"] = { Vector(-31.4, 0.4, -14334), Vector(-4.3, 16167.5, -14336), Vector(0, 0, -12914)}
+MAP_BOUNDS["gm_warmap_v5_snow"] = { Vector(-31.4, 0.4, -14334), Vector(-4.3, 16167.5, -14336), Vector(0, 0, -12914)}
+MAP_BOUNDS["gm_warmap_v5_night"] = { Vector(-31.4, 0.4, -14334), Vector(-4.3, 16167.5, -14336), Vector(0, 0, -12914)}
 
 MAP_PATHS = {}
 MAP_PATHS["gm_flatgrass"]    			  = { Vector(-13466,9319,-12795),
