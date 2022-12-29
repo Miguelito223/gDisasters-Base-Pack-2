b68eda6820e75a7039ffa56e3670422c7820340d
Miguelito223 <miguelitojimenez650@gmail.com>
2022-12-29 12:26:51 +0100

    lel

--- lua/gdisasters/player/process_oxygen.lua
+++ lua/gdisasters/player/process_oxygen.lua
@@ -3,13 +3,10 @@ if (SERVER) then
     function gDisasters_ProcessOxygen()
         if GetConVar("gdisasters_hud_oxygen_enable"):GetInt() == 0 then return end 
         
-        for k, v in pairs(player.GetAll()) do 
-        
-            v:SetNWFloat("BodyOxygen", v.gDisasters.Body.Oxygen)
-            
+        for k, v in pairs(player.GetAll()) do
             if isinWater(v) or isinLava(v) then 
-                v.gDisasters.Body.Oxygen = math.Clamp(v.gDisasters.Body.Oxygen - engine.TickInterval(), 0,10) 
-            
+                v.gDisasters.Body.Oxygen = math.Clamp(v.gDisasters.Body.Oxygen - 0.01, 0,10) 
+                
                 if v.gDisasters.Body.Oxygen <= 0 then
                 
                     if GetConVar("gdisasters_hud_oxygen_damage"):GetInt() == 0 then return end
@@ -26,6 +23,7 @@ if (SERVER) then
             else
                 v.gDisasters.Body.Oxygen = math.Clamp(v.gDisasters.Body.Oxygen + 0.1, 0,10)
             end
+            v:SetNWFloat("BodyOxygen", v.gDisasters.Body.Oxygen)
         end
         for k, v in pairs(ents.FindByClass("npc_*")) do           
             if isinWater(v) or isinLava(v) then 
