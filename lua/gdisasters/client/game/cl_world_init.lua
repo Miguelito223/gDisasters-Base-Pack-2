8c1c10384c1d206fedb35a4d4334f689cfaf5148
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-02-14 12:00:52 +0100

    egh

--- lua/gdisasters/client/game/cl_world_init.lua
+++ lua/gdisasters/client/game/cl_world_init.lua
@@ -4,3 +4,20 @@ hook.Add( "InitPostEntity", "gDisastersFirstJoinLightmaps", function()
 
 end )
 
+hook.Add( "CalcView", "gdisastersCalcView", function( pl, pos, ang, fov, nearZ, farZ )
+	
+	gDisasters.DayNightSystem.CalcView(pl, pos, ang, fov, nearZ, farZ)
+	
+end );
+
+hook.Add( "RenderScene", "gDisastersRenderScene", function( origin, angles, fov )
+
+	gDisasters.DayNightSystem.RenderScene(origin, angles, fov)
+
+end );
+
+hook.Add( "PostDrawSkyBox", "gDisastersPostDrawSkyBox", function()
+
+	gDisasters.DayNightSystem.RenderMoon()
+
+end)
--- lua/gdisasters/shared/game/sh_world_init.lua
+++ lua/gdisasters/shared/game/sh_world_init.lua
@@ -447,24 +447,6 @@ gDisasters.DayNightSystem.RenderMoon = function()
 end
 
 
-hook.Add( "CalcView", "gdisastersCalcView", function( pl, pos, ang, fov, nearZ, farZ )
-	
-	gDisasters.DayNightSystem.CalcView(pl, pos, ang, fov, nearZ, farZ)
-	
-end );
-
-hook.Add( "RenderScene", "gDisastersRenderScene", function( origin, angles, fov )
-
-	gDisasters.DayNightSystem.RenderScene(origin, angles, fov)
-
-end );
-
-hook.Add( "PostDrawSkyBox", "gDisastersPostDrawSkyBox", function()
-
-	gDisasters.DayNightSystem.RenderMoon()
-
-end)
-
 concommand.Add("gdisasters_smite", function()
 
 	local bounds    = getMapSkyBox()
