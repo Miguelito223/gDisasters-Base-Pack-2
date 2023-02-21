5f577f1bb8cb5d04a659b2a0cc88b0c571fc9c1a
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-02-21 19:46:43 +0100

    fixed

---
+++ lua/entities/gd_fog.lua
@@ -0,0 +1,87 @@
+AddCSLuaFile();
+
+ENT.Base = "base_entity";
+ENT.Type = "point";
+ENT.DisableDuplicator = true;
+
+function ENT:Initialize()
+
+    if SERVER then return end
+
+    self.Keys = {
+        "FogStart",
+        "FogEnd",
+        "FogDensity",
+        "FogColor"
+    };
+
+    self.Values = {};
+
+    for k,v in pairs( self.Keys ) do
+
+        self["Get" .. tostring(v)] = function() return self.Values[tostring(v)] end
+        self["Set" .. tostring(v)] = function( ent, value ) self.Values[tostring(v)] = value end
+
+    end
+
+    -- defaults
+    self:SetFogStart( 0.0 );
+    self:SetFogEnd( 0.0 );
+    self:SetFogDensity( 0.0 );
+    self:SetFogColor( Vector( 0.6, 0.7, 0.8 ) );
+
+    hook.Add( "SetupWorldFog", self, self.SetupWorldFog );
+    hook.Add( "SetupSkyboxFog", self, self.SetupSkyFog );
+
+end
+
+function ENT:GetFogValues()
+
+    local tbl = {
+        FogStart = self:GetFogStart(),
+        FogEnd = self:GetFogEnd(),
+        FogDensity = self:GetFogDensity(),
+        FogColor = self:GetFogColor()
+    };
+
+    return tbl;
+
+end
+
+function ENT:Think()
+
+    if ( gd_Fog != self ) then
+
+        gd_Fog = self;
+
+    end
+
+end
+
+function ENT:SetupWorldFog()
+
+    render.FogMode( 1 );
+    render.FogStart( self:GetFogStart() );
+    render.FogEnd( self:GetFogEnd() );
+    render.FogMaxDensity( self:GetFogDensity() );
+
+    local col = self:GetFogColor();
+    render.FogColor( col.x * 255, col.y * 255, col.z * 255 );
+
+    return true;
+
+end
+
+function ENT:SetupSkyFog( scale )
+
+    render.FogMode( 1 );
+    render.FogStart( self:GetFogStart() * scale );
+    render.FogEnd( self:GetFogEnd() * scale );
+    render.FogMaxDensity( self:GetFogDensity() );
+
+    local col = self:GetFogColor();
+    render.FogColor( col.x * 255, col.y * 255, col.z * 255 );
+
+    return true;
+
+end
--- lua/gdisasters/shared/game/sh_world_init.lua
+++ lua/gdisasters/shared/game/sh_world_init.lua
@@ -212,6 +212,7 @@ gDisasters.DayNightSystem.initEntities_Function = function()
 		gDisasters.DayNightSystem.EnvFogController = ents.FindByClass( "env_fog_controller" )[1]
 		gDisasters.DayNightSystem.RelayDawn = ents.FindByName( "dawn" )[1]
 		gDisasters.DayNightSystem.RelayDusk = ents.FindByName( "dusk" )[1]
+		gDisasters.DayNightSystem.Fog = ents.Create("gd_fog")
 		
 
 		if IsValid(gDisasters.DayNightSystem.EnvSun) then
@@ -355,6 +356,7 @@ gDisasters.DayNightSystem.Think = function()
 		local frac = 0;
 		local ease = 0.3;
 
+
 		if ( gDisasters.DayNightSystem.Time >= gDisasters.DayNightSystem.InternalVars.time.Dawn_Start and gDisasters.DayNightSystem.Time < dawnMidPoint ) then
 			cur = gDisasters.DayNightSystem.InternalVars.SkyPaint.Night;
 			next = gDisasters.DayNightSystem.InternalVars.SkyPaint.Dawn;
@@ -385,7 +387,12 @@ gDisasters.DayNightSystem.Think = function()
 			curfog = gDisasters.DayNightSystem.InternalVars.Fog.Day;
 			nextfog = gDisasters.DayNightSystem.InternalVars.Fog.Day;
 		end
-		
+		if (CLIENT) then
+			gDisasters.DayNightSystem.Fog:SetFogStart(Lerp(frac, cur.FogStart, next.FogStart))
+			gDisasters.DayNightSystem.Fog:SetFogEnd(Lerp(frac, cur.FogEnd, next.FogEnd))
+			gDisasters.DayNightSystem.Fog:SetFogDensity(Lerp(frac, cur.FogDensity, next.FogDensity))
+			gDisasters.DayNightSystem.Fog:SetFogColor(LerpVector(frac, cur.FogColor, next.FogColor))
+		end		
 		if (SERVER) then
 
 			gDisasters.DayNightSystem.EnvSkyPaint:SetTopColor( LerpVector( frac, cur.TopColor, next.TopColor ) );
