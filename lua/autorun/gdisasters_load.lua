38551aac6830f49767ee9e99eccd1ff811e42a51
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-03-11 08:53:39 +0100

    fixed

--- lua/autorun/gdisasters_load.lua
+++ lua/autorun/gdisasters_load.lua
@@ -15,8 +15,8 @@ include("gdisasters/shared/game/sh_decals.lua")
 
 if (SERVER) then
 
-	AddCSLuaFile("gdisasters/server/shared_func/sv_main.lua")
-	AddCSLuaFile("gdisasters/server/shared_func/sv_netstrings.lua")
+	AddCSLuaFile("gdisasters/server/server_func/sv_main.lua")
+	AddCSLuaFile("gdisasters/server/server_func/sv_netstrings.lua")
 	AddCSLuaFile("gdisasters/server/game/antilag/sv_main.lua")
 	AddCSLuaFile("gdisasters/server/player-npcs/sv_process_oxygen.lua")
 	AddCSLuaFile("gdisasters/server/game/sv_autospawn.lua")
@@ -30,8 +30,8 @@ if (SERVER) then
 	AddCSLuaFile("gdisasters/server/extensions/sv_patchs-bounds.lua")
 
 	include("gdisasters/server/extensions/sv_patchs-bounds.lua")
-	include("gdisasters/server/shared_func/sv_main.lua")
-	include("gdisasters/server/shared_func/sv_netstrings.lua")
+	include("gdisasters/server/server_func/sv_main.lua")
+	include("gdisasters/server/server_func/sv_netstrings.lua")
 	include("gdisasters/server/atmosphere/sv_main.lua")
 	include("gdisasters/server/player-npcs/sv_process_gfx.lua")
 	include("gdisasters/server/player-npcs/sv_process_temp.lua")
@@ -49,8 +49,8 @@ if (CLIENT) then
 
 	AddCSLuaFile("autorun/client/garrys_particles.lua")
 
-	AddCSLuaFile("gdisasters/client/shared_func/cl_main.lua")
-	AddCSLuaFile("gdisasters/client/shared_func/cl_netstrings.lua")
+	AddCSLuaFile("gdisasters/client/client_func/cl_main.lua")
+	AddCSLuaFile("gdisasters/client/client_func/cl_netstrings.lua")
 	AddCSLuaFile("gdisasters/client/spawnlist/menu/cl_main.lua")
 	AddCSLuaFile("gdisasters/client/spawnlist/menu/cl_populate.lua")
 	AddCSLuaFile("gdisasters/client/toolmenu/cl_menu.lua")
@@ -63,8 +63,8 @@ if (CLIENT) then
 
 	include("gdisasters/client/game/cl_world_init.lua")
 	include("gdisasters/client/atmosphere/cl_main.lua")
-	include("gdisasters/client/shared_func/cl_main.lua")
-	include("gdisasters/client/shared_func/cl_netstrings.lua")
+	include("gdisasters/client/client_func/cl_main.lua")
+	include("gdisasters/client/client_func/cl_netstrings.lua")
 	include("gdisasters/client/player-npcs/cl_process_gfx.lua")
 	include("gdisasters/client/player-npcs/cl_process_temp.lua")
 	include("gdisasters/client/player-npcs/cl_postspawn.lua")
