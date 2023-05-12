5cce958e6305904cc4edd83fd6afaaf00cf158af
Miguelito223 <miguelitojimenez650@gmail.com>
2023-05-12 08:16:31 +0200

    xd

--- lua/entities/gd_d8_ancientvolcano.lua
+++ lua/entities/gd_d8_ancientvolcano.lua
@@ -308,8 +308,9 @@ function ENT:SpawnFunction( ply, tr )
 
 	self.OWNER = ply
 	local ent = ents.Create( self.ClassName )
+	local scale = self:GetModelScale()
 	ent:SetPhysicsAttacker(ply)
-	ent:SetPos( tr.HitPos + tr.HitNormal * 920  ) 
+	ent:SetPos( tr.HitPos + tr.HitNormal * (920 * scale)  ) 
 	ent:Spawn()
 	ent:Activate()
 	return ent
