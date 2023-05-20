e077feefc4b1bd13fabe8c80022b995568bec041
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-05-20 13:02:25 +0200

    eh

--- lua/entities/gd_npc_nurse.lua
+++ lua/entities/gd_npc_nurse.lua
@@ -355,6 +355,13 @@ function ENT:UpdateTransmitState()
 	return TRANSMIT_ALWAYS
 end
 
+list.Set( "NPC", "gd_npc_nurse", {
+	Name = "Bcat lol", 
+	Class = "gd_npc_nurse", 
+	Category = "Nextbot",
+	AdminOnly = false,
+})
+
 
 
 
