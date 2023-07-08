7bcdaafce0555f13df13d051cf2a8a3260b5bed8
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-07-08 12:01:25 +0200

    e

--- lua/gdisasters/__shared/_shared_func/sh_main.lua
+++ lua/gdisasters/__shared/_shared_func/sh_main.lua
@@ -4,7 +4,7 @@ gDisasters.Cached         = {}
 gDisasters.DayNightSystem        = {}
 gDisasters.DayNightSystem.InternalVars = {}
 gDisasters.Game                  = {}
-gDisasters.Version = 0.3
+gDisasters.Version = 0.31
 gDisasters.WorkshopURL = "https://steamcommunity.com/sharedfiles/filedetails/changelog/2522900784"
 
 Break_Sounds = {
