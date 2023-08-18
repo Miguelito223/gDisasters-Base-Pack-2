a31a75c8cad2eb2502e47ea6cfc8c2626e6756ec
Miguelito223 <miguelitojimenez650@gmail.com>
2023-08-18 09:57:49 +0200

    eh

--- lua/entities/gd_d10_supervolcano.lua
+++ lua/entities/gd_d10_supervolcano.lua
@@ -16,7 +16,7 @@ ENT.AutomaticFrameAdvance            = true
 
 function ENT:Initialize()	
 	self:DrawShadow(false)
-	self:SetModelScale(2,0.0000001)
+	self:SetModelScale(2,0.000001)
 	
 	if (SERVER) then
 		
--- lua/entities/gd_d2_minivolcano.lua
+++ lua/entities/gd_d2_minivolcano.lua
@@ -16,7 +16,7 @@ ENT.AutomaticFrameAdvance            = true
 
 function ENT:Initialize()	
 	self:DrawShadow( false)
-	self:SetModelScale(0.5,0.0000001)
+	self:SetModelScale(0.5,0.000001)
 
 	
 	if (SERVER) then
