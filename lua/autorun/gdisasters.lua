c976112c479fee756a7475934aea94feb2240e49
Miguelito223 <miguelitojimenez650@gmail.com>
2023-08-24 15:29:03 +0200

    e

--- lua/autorun/gdisasters.lua
+++ lua/autorun/gdisasters.lua
@@ -99,7 +99,7 @@ gDisasters:Msg("FINISH")
 
 gDisasters:Msg("LOADING DECALS...")
 
-gDisasters.root_Directory = "materials/decals" 
+gDisasters.root_Directory = "materials/decals/gdisasters" 
 
 function gDisasters:AddDecalsFile( File, directory )
 	local name = File:match("(.+)%..+$")
