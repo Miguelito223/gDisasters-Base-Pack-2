e7a0634c657320f9d4ed142a4bef0638232a38d9
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-11-12 15:00:38 +0100

    e

--- lua/autorun/gdisasters.lua
+++ lua/autorun/gdisasters.lua
@@ -9,6 +9,7 @@ for _,v in pairs(engine.GetAddons()) do
 	if v.downloaded and tonumber(v.wsid) == 2522900784 or v.title = "gDisasters Revived"
 		gDisasters.WorkshopVersion = true
 		print("yes workshop")
+		break
 	else
 		print("no workshop")
 		return
