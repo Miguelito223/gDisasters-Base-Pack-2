237cee582beee47b26fd4913be791a1a581e864e
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-11-12 14:35:25 +0100

    e

--- lua/autorun/gdisasters.lua
+++ lua/autorun/gdisasters.lua
@@ -1,7 +1,7 @@
 --class
 gDisasters = {}
 gDisasters.__index = gDisasters
-gDisasters.Version = 0.36
+gDisasters.Version = 0.361
 gDisasters.WorkShopURL = "https://steamcommunity.com/sharedfiles/filedetails/?id=2522900784"
 
 for k,v in pairs(engine.GetAddons()) do
