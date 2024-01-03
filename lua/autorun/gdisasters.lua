d5146d4171983c8c40bf40fb51016f00f7f486a7
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-01-03 15:31:08 +0100

    eh

--- lua/autorun/gdisasters.lua
+++ lua/autorun/gdisasters.lua
@@ -1,7 +1,7 @@
 --class
 gDisasters = {}
 gDisasters.__index = gDisasters
-gDisasters.Version = 0.38
+gDisasters.Version = 0.381
 gDisasters.WorkShopURL = "https://steamcommunity.com/sharedfiles/filedetails/?id=2522900784"
 gDisasters.WorkshopVersion = false
 
