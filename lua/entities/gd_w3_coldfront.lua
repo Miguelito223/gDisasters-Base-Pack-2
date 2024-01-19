75aea782a8f46f240a932c09dfbfca4c7860f838
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-01-19 16:06:49 +0100

    eh

--- lua/entities/gd_w3_coldfront.lua
+++ lua/entities/gd_w3_coldfront.lua
@@ -148,25 +148,37 @@ function ENT:Phase()
 	end)
 
 	timer.Simple(30, function()
-		next_state = "light_rain_fading" 
+		next_state = "light_rain_fading"
+
+		if self.State != next_state then self:OnStateChange(next_state) end
+
 		self.State = next_state 
 		self:StateProcessor()
 	end)
 
 	timer.Simple(40, function()
 		next_state = "medium_wind"
+
+		if self.State != next_state then self:OnStateChange(next_state) end
+
 		self.State = next_state 
 		self:StateProcessor()
 	end)
 
 	timer.Simple(60, function()
 		next_state = "heavy_wind" 
+
+		if self.State != next_state then self:OnStateChange(next_state) end
+
 		self.State = next_state 
 		self:StateProcessor()
 	end)
 
 	timer.Simple(90, function()
 		next_state = "dead" 
+		
+		if self.State != next_state then self:OnStateChange(next_state) end
+
 		self.State = next_state 
 		self:StateProcessor()
 	end)
--- lua/entities/gd_w4_strong_coldfront.lua
+++ lua/entities/gd_w4_strong_coldfront.lua
@@ -148,24 +148,36 @@ function ENT:Phase()
 
 	timer.Simple(30, function()
 		next_state = "light_rain_fading" 
+
+		if self.State != next_state then self:OnStateChange(next_state) end
+
 		self.State = next_state 
 		self:StateProcessor()
 	end)
 
 	timer.Simple(40, function()
 		next_state = "medium_wind"
+
+		if self.State != next_state then self:OnStateChange(next_state) end
+
 		self.State = next_state 
 		self:StateProcessor()
 	end)
 
 	timer.Simple(60, function()
-		next_state = "heavy_wind" 
+		next_state = "heavy_wind"
+		
+		if self.State != next_state then self:OnStateChange(next_state) end
+
 		self.State = next_state 
 		self:StateProcessor()
 	end)
 
 	timer.Simple(90, function()
 		next_state = "dead" 
+		
+		if self.State != next_state then self:OnStateChange(next_state) end
+
 		self.State = next_state 
 		self:StateProcessor()
 	end)
